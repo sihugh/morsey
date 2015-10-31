7fa2fc95be22cea9dfeacf18bdbfc8e1fe16d5e8
Simon Hughesdon <simon@hughesdon.com>
2015-10-31 01:31:50 +0000

    Added post end points for translating via html forms

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var Morse = require('../utils/morseHelper'),
+    morse = new Morse();
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -8,11 +11,23 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/alpha', function (req, res, next) {
-    res.render('index', {title: 'Morse Code Converter'});
+    var text,
+        translated;
+
+    text = req.body.morse;
+    translated = morse.convert_to_alpha(text);
+
+    res.render('translated', {title: 'Morse Code Converter', orig: text, translated: translated});
 });
 
 router.post('/morse', function (req, res, next) {
-    res.render('index', {title: 'Morse Code Converter'});
+    var text,
+        translated;
+
+    text = req.body.alpha;
+    translated = morse.convert_to_morse(text);
+
+    res.render('translated', {title: 'Morse Code Converter', orig: text, translated: translated});
 });
 
 module.exports = router;
